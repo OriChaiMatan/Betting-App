4cd185c92eef361fed4e5b1eed2da369f297724e
OriChaiMatan <ori.chaimatan@gmail.com>
2024-11-19 19:54:20 +0200

    Add time&date&stadium data to past match details page

--- betting app front/src/pages/soccer/SoccerPastMatchDetails.jsx
+++ betting app front/src/pages/soccer/SoccerPastMatchDetails.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { gamesService } from "../../services/games.service"
+import { utilService } from "../../services/util.service"
 import { ScoreTable } from "../../cmps/soccer/past-match/ScoreTable"
 import { StickyHeader } from "../../cmps/soccer/StickyHeader"
 import { CallToActionHeader } from "../../cmps/soccer/future-match/CallToActionHeader"
+import { MdOutlinePlace } from "react-icons/md"
+
 
 export function SoccerPastMatchDetails() {
     const [match, setMatch] = useState(null)
@@ -62,6 +65,16 @@ export function SoccerPastMatchDetails() {
                         <img src={match.team_away_badge} alt="Home team badge" />
                         <span>Away Team</span>
                         <h3 className='heading-tertiary'>{match.match_awayteam_name}</h3>
+                    </div>
+                </div>
+                <div className="place-data">
+                    <div className="stadium">
+                        <MdOutlinePlace />
+                        <h3 className="heading-tertiary">{match.match_stadium}</h3>
+                    </div>
+                    <div className="date">
+                        <h3 className='heading-tertiary'>{match.match_time}</h3>
+                        <h3 className='heading-tertiary'>{utilService.formatDate(match.match_date)}</h3>
                     </div>
                 </div>
                 <div className="statistic-data">
