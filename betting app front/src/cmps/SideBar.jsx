3ddc3a9a5c3b7dd212f22dbb9e11f6651b8bc10f
OriChaiMatan <ori.chaimatan@gmail.com>
2024-12-05 18:24:31 +0200

    Fix Sidebar active class

--- betting app front/src/cmps/SideBar.jsx
+++ betting app front/src/cmps/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { FaHome } from "react-icons/fa"
 import { HiCubeTransparent } from "react-icons/hi"
 import { CgSmartphoneChip } from "react-icons/cg"
@@ -13,34 +13,34 @@ export function SideBar({ onToggleSidebar }) {
     return (
         <div className='side-bar'>
             <div className="first-routes">
-                <Link to={'/'} className='link-info' activeClassName="active-link" onClick={onToggleSidebar}>
+                <NavLink to={'/'} className={({ isActive }) => isActive ? 'link-info active-link' : 'link-info'} onClick={onToggleSidebar}>
                     <FaHome className='icon' />
                     <span>Home</span>
-                </Link>
-                <Link to={'/bet'} className='link-info' activeClassName="active-link" onClick={onToggleSidebar}>
+                </NavLink>
+                <NavLink to={'/bet'} className={({ isActive }) => isActive ? 'link-info active-link' : 'link-info'} onClick={onToggleSidebar}>
                     <HiCubeTransparent className='icon' />
                     <span>Betting</span>
-                </Link>
-                <Link to={'/ai-assistant'} className='link-info' activeClassName="active-link" onClick={onToggleSidebar} >
+                </NavLink>
+                <NavLink to={'/ai-assistant'} className={({ isActive }) => isActive ? 'link-info active-link' : 'link-info'} onClick={onToggleSidebar} >
                     <CgSmartphoneChip className='icon' />
                     <span>AI Recommendations</span>
-                </Link>
+                </NavLink>
             </div>
             <div className="second-routes">
                 <h1>Soccer</h1>
                 <div className="links">
-                    <Link to={'/leagues'} className='link-info' activeClassName="active-link" onClick={onToggleSidebar}>
+                    <NavLink to={'/leagues'} className={({ isActive }) => isActive ? 'link-info active-link' : 'link-info'} onClick={onToggleSidebar}>
                         <PiSoccerBallFill className='icon' />
                         <span>Leagues</span>
-                    </Link>
-                    <Link to={'/future-match'} className='link-info' activeClassName="active-link" onClick={onToggleSidebar}>
+                    </NavLink>
+                    <NavLink to={'/future-match'} className={({ isActive }) => isActive ? 'link-info active-link' : 'link-info'} onClick={onToggleSidebar}>
                         <GiSoccerField className='icon' />
                         <span>Future Matches</span>
-                    </Link>
-                    <Link to={'/past-match'} className='link-info' activeClassName="active-link" onClick={onToggleSidebar}>
+                    </NavLink>
+                    <NavLink to={'/past-match'} className={({ isActive }) => isActive ? 'link-info active-link' : 'link-info'} onClick={onToggleSidebar}>
                         <FaHistory className='icon' />
                         <span>Previous Matches</span>
-                    </Link>
+                    </NavLink>
                 </div>
             </div>
         </div>
